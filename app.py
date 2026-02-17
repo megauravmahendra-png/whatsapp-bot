29f8c67f18951a9087ba302fa185011c7fed40fc
megauravmahendra-png <megauravmahendra@gmail.com>
2026-02-18 01:39:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -198,5 +198,5 @@ def home():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
